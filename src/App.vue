0f12a00744da04ab37896966499ade1b62410428
souloppen <arieloppenheimer@outlook.cl>
2024-10-11 09:31:27 -0300

    move:fetch from home to app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <script setup>
-import {RouterView } from 'vue-router'
 import NavComponent from '@/components/NavComponent.vue'
+import {onMounted} from 'vue';
+import {RouterView } from 'vue-router'
+import { useStore } from 'vuex';
+const store=useStore();
+const cargarCursos=async()=>{
+  try {
+          const response = await fetch('/cursos.json');
+
+          if (!response.ok) {
+              throw new Error('Error al cargar el archivo JSON');
+          }
+
+          const data = await response.json();
+
+          const listaCursos = data.cursos;
+          for(let curso of listaCursos){
+            store.dispatch('agregarFetch', curso);
+          }
+          store.dispatch('changeStatus')
+    } catch (error) {
+        console.error('Hubo un problema con la solicitud fetch:', error);
+    }
+};
+onMounted(()=>cargarCursos());
 </script>
 
 <template>
