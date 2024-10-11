9915bf72a7623c1c79679781370aaa2c8d2dbf16
souloppen <arieloppenheimer@outlook.cl>
2024-10-11 15:35:12 -0300

    add:add form

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -1,20 +1,142 @@
 <script setup>
 import TableItemComponent from '@/components/TableItemComponent.vue';
 import CardInfoComponent from '@/components/CardInfoComponent.vue';
-import {computed} from 'vue'
+import {ref,computed} from 'vue'
 import { useStore } from 'vuex';
 const store=useStore();
+const agregar=ref(false)
 const cursos=computed(()=>store.state.courses)
 const cupos=computed(()=>store.getters.totalCupos)
 const inscritos=computed(()=>store.getters.totalInscrito)
 const cursosActivos=computed(()=>store.getters.totalCursosActivos)
+const mName=ref('');
+const mUrl=ref('');
+const mCupos=ref('');
+const mInscritos=ref('');
+const mDuracion=ref('');
+const mDate=ref('');
+const mValue=ref('');
+const mDescription=ref('');
+const clean=()=>{
+  mName.value='';
+  mUrl.value='';
+  mCupos.value='';
+  mInscritos.value='';
+  mDuracion.value='';
+  mDate.value='';
+  mValue.value='';
+  mDescription.value='';
+}
+const add=()=>{
+  const id= Math.ceil(Math.random()*1000);
+  const obj={
+        id,
+        img: mUrl.value,
+        nombre:mName.value,
+        costo:mValue.value,
+        duracion: mDuracion.value,
+        cupos: mCupos.value,
+        inscritos: mInscritos.value,
+        completado: false,
+        fecha_registro: mDate.value,
+        descripcion: mDescription.value
+      }
+  console.log(obj)
+  clean();
+  }
+const cancelar=()=>{
+  clean();
+  agregar.value=false
+  }
 </script>
 
 <template>
   <main>
     <h1 class="text-center">Administración</h1>
-    <VBtn  color="blue-darken-4" class="d-block w-25 mx-auto">Agregar Curso</VBtn>
-    <v-table class="d-block w-75 mx-auto">
+    <VBtn  color="blue-darken-4" class="d-block w-25 mx-auto" @click="agregar=true">Agregar Curso</VBtn>
+    <VDialog
+      v-model="agregar"
+      class="w-50"
+      persistent
+    >
+      <VCard>
+        <VCardTitle class="text-blue-darken-4">Agregar Curso</VCardTitle>
+        <VForm>
+          <VTextField
+            label="Nombre"
+            class="d-block w-75 mx-auto"
+            variant="underlined"
+            color="blue-darken-4"
+            v-model="mName"
+          >
+          </VTextField>
+          <VTextField
+            label="URL de la imagen"
+            class="d-block w-75 mx-auto"
+            variant="underlined"
+            color="blue-darken-4"
+            v-model="mUrl"
+          >
+          </VTextField>
+          <VTextField
+            label="Cupos del curso"
+            class="d-block w-75 mx-auto"
+            variant="underlined"
+            color="blue-darken-4"
+            type="number"
+            v-model="mCupos"
+          >
+          </VTextField>
+          <VTextField
+            label="Inscritos del curso"
+            class="d-block w-75 mx-auto"
+            variant="underlined"
+            color="blue-darken-4"
+            type="number"
+            v-model="mInscritos"
+          >
+          </VTextField>
+          <VTextField
+            label="Duración del curso"
+            class="d-block w-75 mx-auto"
+            variant="underlined"
+            color="blue-darken-4"
+            v-model="mDuracion"
+          >
+          </VTextField>
+          <VTextField
+            label="Fecha de registro"
+            class="d-block w-75 mx-auto"
+            variant="underlined"
+            color="blue-darken-4"
+            v-model="mDate"
+          >
+          </VTextField>
+          <VTextField
+            label="Costo del curso"
+            class="d-block w-75 mx-auto"
+            variant="underlined"
+            color="blue-darken-4"
+            type="number"
+            v-model="mValue"
+          ></VTextField>
+          <VTextarea
+          label="Descripción"
+          variant="underlined"
+          color="blue-darken-4"
+          class="d-block w-75 mx-auto"
+          v-model="mDescription"
+          no-resize>
+          </VTextarea>
+          <VCardActions class="d-block w-75 mx-auto pa-0">
+            <VBtn class="bg-green" @click="add">Agregar</VBtn>
+            <VBtn class="bg-yellow" @click="clean">Limpiar</VBtn>
+            <VBtn class="bg-red" @click="cancelar">Cancelar</VBtn>
+          </VCardActions>
+        </VForm>
+      </VCard>
+    </VDialog>
+    <VTable class="d-block w-75 mx-auto">
     <thead>
       <tr>
         <th class="text-left">
@@ -43,10 +165,17 @@ const cursosActivos=computed(()=>store.getters.totalCursosActivos)
         </th>
       </tr>
     </thead>
-    <tbody>
+    <tbody v-if="cursos.length>0">
       <TableItemComponent v-for="curso in cursos" :key="curso.id" :id="curso.id"/>
     </tbody>
-  </v-table>
+    <tbody v-else>
+      <tr>
+        <td  class="text-center font-weight-bold" colspan="8">
+          No existen cursos
+        </td>
+      </tr>
+    </tbody>
+  </VTable>
   <VDivider class="d-block w-75 mx-auto"></VDivider>
   <CardInfoComponent color="text-blue-darken-4" icon="account-multiple">
     <p>Cantidad total de alumnos permitidos: {{ cupos }} alumnos.</p>
