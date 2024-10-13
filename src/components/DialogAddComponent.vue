f0717b4b27f980cd145e9932ae6de8c12845b825
souloppen <arieloppenheimer@outlook.cl>
2024-10-13 17:36:23 -0300

    add:rules

--- src/components/DialogAddComponent.vue
+++ src/components/DialogAddComponent.vue
@@ -1,8 +1,27 @@
 <script setup>
-import {reactive} from 'vue'
+import {ref,reactive} from 'vue'
 import { useStore } from 'vuex';
 const store=useStore();
 const emits=defineEmits(['cerrarAgregar']);
+const textRules=[
+  v => !!v || 'Propiedad requerida',
+  v => (v.length >= 3) || 'Min 3 caracteres',
+  v => (v.length <= 10) || 'Max 10 caracteres'
+]
+const numberRules=[
+  v => !!v || 'Propiedad requerida',
+  v => (Number.isInteger(Number(v))) || 'Número tiene que se entero'
+]
+const maxPersonas=v => (v<=999) || 'max de personas 999'
+const inscritoMenorACupos=v => (Number(v)<=obj.cupos) || 'Inscritos tiene que ser menos que cupos'
+const dateFormatRule = v => {
+  const datePattern = /^(0[1-9]|1[0-9]|2[0-9]|3[01])\/(0[1-9]|1[0-2])\/(19|20)\d\d$/;
+  return datePattern.test(v) || 'Debe ser una fecha en formato dd/mm/yyyy';
+}
+const maxCosto=v => (Number(v)<=10000000) || 'max costo 10000000'
+
+const minCosto=v => (v>=10000) || 'min costo 10000'
+const valid = ref(false);
 let obj=reactive(
   {
     "id": "",
@@ -25,11 +44,18 @@ const clean=()=>{
 }
 
 const add=()=>{
+  console.log(valid)
+  if (valid.value) {
   obj.id= Math.ceil(Math.random()*1000);
   obj.completado=false
   store.dispatch('agregar',obj)
   emits('cerrarAgregar')
+  alert('add')
   }
+  else{
+    alert(valid.value)
+  }
+}
 const cancelar=()=>{
   emits('cerrarAgregar')
   }
@@ -38,12 +64,14 @@ const cancelar=()=>{
 
       <VCard>
         <VCardTitle class="text-blue-darken-4">Agregar Curso</VCardTitle>
-        <VForm>
+        <VForm v-model="valid">
           <VTextField
             label="Nombre"
             class="d-block w-75 mx-auto"
             variant="underlined"
             color="blue-darken-4"
+            :rules="textRules"
+            required
             v-model="obj.nombre"
           >
           </VTextField>
@@ -52,6 +80,7 @@ const cancelar=()=>{
             class="d-block w-75 mx-auto"
             variant="underlined"
             color="blue-darken-4"
+            required
             v-model="obj.img"
           >
           </VTextField>
@@ -61,6 +90,8 @@ const cancelar=()=>{
             variant="underlined"
             color="blue-darken-4"
             type="number"
+            :rules="[...numberRules,maxPersonas]"
+            required
             v-model="obj.cupos"
           >
           </VTextField>
@@ -70,6 +101,8 @@ const cancelar=()=>{
             variant="underlined"
             color="blue-darken-4"
             type="number"
+            :rules="[...numberRules,maxPersonas,inscritoMenorACupos]"
+            required
             v-model="obj.inscritos"
           >
           </VTextField>
@@ -78,6 +111,8 @@ const cancelar=()=>{
             class="d-block w-75 mx-auto"
             variant="underlined"
             color="blue-darken-4"
+            :rules="textRules"
+            required
             v-model="obj.duracion"
           >
           </VTextField>
@@ -86,6 +121,8 @@ const cancelar=()=>{
             class="d-block w-75 mx-auto"
             variant="underlined"
             color="blue-darken-4"
+            :rules="[...textRules,dateFormatRule]"
+            required
             v-model="obj.fecha_registro"
           >
           </VTextField>
@@ -95,6 +132,7 @@ const cancelar=()=>{
             variant="underlined"
             color="blue-darken-4"
             type="number"
+            :rules="[...numberRules,maxCosto,minCosto]"
             v-model="obj.costo"
           ></VTextField>
           <VTextarea
@@ -106,7 +144,7 @@ const cancelar=()=>{
           no-resize>
           </VTextarea>
           <VCardActions class="d-block w-75 mx-auto pa-0">
-            <VBtn class="bg-green" @click="add">Agregar</VBtn>
+            <VBtn class="bg-green" :disabled="!valid" @click="add">Agregar</VBtn>
             <VBtn class="bg-yellow" @click="clean">Limpiar</VBtn>
             <VBtn class="bg-red" @click="cancelar">Cancelar</VBtn>
           </VCardActions>
